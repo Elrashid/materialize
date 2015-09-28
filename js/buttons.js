8f3302f7b847ad726459676502f98128b9325b7e
Elrashid <Elrashid@users.noreply.github.com>
2015-09-28 06:59:07 +0400

    fixed action btn
    1)
    the Buttons  is clickable if only =>   opacity: "0" 
    add => display: "none"  => to the animation  
    
    2)
    add =>  var $this = $(this); => to closeFAB function 
    so i can call the closeFAB to add =>  display: "none"   
    => when first time page loaded 
     =>
      $(document).ready(function(){
          $('.fixed-action-btn').closeFAB();
       });

--- js/buttons.js
+++ js/buttons.js
@@ -23,6 +23,7 @@
       openFABMenu($this);
     },
     closeFAB: function() {
+       var $this = $(this);
       closeFABMenu($this);
     }
   });
@@ -34,12 +35,14 @@
       $this.addClass('active');
       $this.find('ul .btn-floating').velocity(
         { scaleY: ".4", scaleX: ".4", translateY: "40px"},
+        { display: "none" },
         { duration: 0 });
 
       var time = 0;
       $this.find('ul .btn-floating').reverse().each(function () {
         $(this).velocity(
           { opacity: "1", scaleX: "1", scaleY: "1", translateY: "0"},
+          { display: "block" },
           { duration: 80, delay: time });
         time += 40;
       });
@@ -53,6 +56,7 @@
     $this.find('ul .btn-floating').velocity("stop", true);
     $this.find('ul .btn-floating').velocity(
       { opacity: "0", scaleX: ".4", scaleY: ".4", translateY: "40px"},
+      { display: "none" },
       { duration: 80 }
     );
   };
